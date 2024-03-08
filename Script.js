4239d3b9b8078624735194de9e72cfe9859d6139
lauvivien <vivien.lau@iubh-fernstudium.de>
2024-03-08 21:53:27 +0100

    Back to top button

--- Script.js
+++ Script.js
@@ -13,3 +13,19 @@ accordionItemHeaders.forEach(accordionItemHeader => {
    
   });
 });
+let mybutton = document.getElementById("BTT");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
